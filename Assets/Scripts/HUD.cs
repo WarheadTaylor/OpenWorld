b22627be832ad70b1f4a3b96bd9c6a8253bac6a0
pyrohail <pyrohail.ca@gmail.com>
2014-01-27 16:44:55 -0500

    Initial commit

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUD : MonoBehaviour {
+	private int frameCount = 0;
+	private float dt = 0.0F;
+	private float fps = 0.0F;
+	private float updateRate = 4.0F;
+
+	void Start () {
+
+	}
+
+	void Update () {
+		frameCount++;
+		dt += Time.deltaTime;
+		if (dt > 1.0F / updateRate) {
+			fps = frameCount / dt ;
+			frameCount = 0;
+			dt -= 1.0F / updateRate;
+		}
+	}
+
+	void OnGUI () {
+		GUI.Label(new Rect (10,10,150,100), "Current FPS: "+ ((int)fps).ToString());
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	private CharacterController controller;
+	private Transform firstPersonCamera;
+
+	private Vector3 moveDirection;
+
+	private float movementSpeed = 3.0F;
+	private const float regularSpeed = 3.0F;
+	private const float sprintSpeed = 5.0F;
+	private const float jumpSpeed = 5.0F;
+	private const float gravity = 9.81F;
+	private const float mouseSensitivity = 1.0F;
+	private float verticalRotation = 0.0F;
+
+	void Start() {
+		controller = GetComponent<CharacterController>();
+		firstPersonCamera = transform.Find("FirstPersonCamera");
+		if (!Application.isEditor) {
+			Screen.showCursor = false;
+		}
+	}
+
+	void Update() {
+		// Movement
+		if (controller.isGrounded) {
+			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * movementSpeed;
+			moveDirection = transform.rotation * moveDirection;
+
+			if (Input.GetButton("Jump")) {
+				moveDirection.y = jumpSpeed;
+			}
+
+			// Control sprinting
+			if (Input.GetKey(KeyCode.LeftShift)) {
+				movementSpeed = sprintSpeed;
+			} else {
+				movementSpeed = regularSpeed;
+			}
+		}
+
+		// Rotation
+		transform.Rotate(0, Input.GetAxis("Mouse X") * mouseSensitivity, 0);
+		verticalRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+		verticalRotation = Mathf.Clamp(verticalRotation, -60.0F, 60.0F);
+		firstPersonCamera.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+}
