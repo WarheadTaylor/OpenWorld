412b34403ccf1a5b58044a1d9c6bfdda655714fb
pyrohail <pyrohail.ca@gmail.com>
2014-01-28 00:59:11 -0500

    Ability for character to push rigid body

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -66,4 +66,18 @@ public class Player : MonoBehaviour {
 		moveDirection.y -= gravity * Time.deltaTime;
 		controller.Move(moveDirection * Time.deltaTime);
 	}
+
+	void OnControllerColliderHit(ControllerColliderHit hit) {
+		Rigidbody body = hit.collider.attachedRigidbody;
+		if (body == null || body.isKinematic) {
+			return;
+		}
+		
+		if (hit.moveDirection.y < -0.3F) {
+			return;
+		}
+		
+		Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+		body.velocity = pushDir * 2.0F;
+	}
 }
