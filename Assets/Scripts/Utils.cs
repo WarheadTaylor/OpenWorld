27c804aaad9b32bb2c5b7ea9eb6fb3b21e22be12
pyrohail <pyrohail.ca@gmail.com>
2014-01-29 18:48:13 -0500

    Fixing taylor's screwups

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -16,6 +16,4 @@ public class Utils : MonoBehaviour {
 			Application.Quit();
 		}
 	}
-	
-
 }
--- Assets/Scripts/Webplayer.cs
+++ Assets/Scripts/Webplayer.cs
@@ -3,19 +3,17 @@ using System.Collections;
 
 public class Webplayer : MonoBehaviour {
 
-	// Use this for initialization
 	void Start () {
 	
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 
 		// Lock mouse to window.
 		Screen.lockCursor = true;
 
-	 if (Application.CanStreamedLevelBeLoaded(1)) {
+		if (Application.CanStreamedLevelBeLoaded(1)) {
 			Application.LoadLevel(1);
 		}
-}
+	}
 }
