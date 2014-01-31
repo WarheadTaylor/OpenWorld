96caf1878dbb2403ce190ec20131a3c92dc3a120
pyrohail <pyrohail.ca@gmail.com>
2014-01-31 04:07:08 -0500

    Able to add item to inventory, and associate an item with a integer value.

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,19 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using SimpleJSON;
 
 public class Inventory : MonoBehaviour {
 	private bool inventoryOpen = false;
 	private int[] inventoryItems;
 
+	private JSONNode listOfItems;
+
 	void Start () {
 		inventoryItems = new int[20];
+		listOfItems = JSON.Parse(Resources.Load<TextAsset>("items").text);
+
+		for (int i = 0; i < inventoryItems.Length; i++) {
+			inventoryItems[i] = -1;
+		}
 	}
 
 	void Update () {
 
 	}
 
-	void onGui () {
-		GUI.Box(new Rect(0,0,640,480), "Inventory");
+	public bool addToInventory(string item) {
+		if (listOfItems[item] == null) {
+			return false;
+		}
+
+		for (int i = 0; i < inventoryItems.Length; i++) {
+			if (inventoryItems[i] == -1) {
+				inventoryItems[i] = listOfItems[item].AsInt;
+
+				Debug.Log (item + " added to position: " + i.ToString());
+
+				break;
+			}
+		}
+
+		return true;
 	}
 }
--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -1,36 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using SimpleJSON;
 
 public class Pickup : MonoBehaviour {
 	private Transform firstPersonCamera;
-	private GameObject objectHeld;
+
+	private GameObject item;
+	private Inventory inventoryScript;
 
 	void Start () {
 		firstPersonCamera = transform.Find("FirstPersonCamera");
+		inventoryScript = transform.GetComponent<Inventory>();
 	}
 
 	void Update () {
 		// Ability to pickup GameObject's
 		if (Input.GetKeyDown(KeyCode.E)) {
-			if (!objectHeld) {
-				RaycastHit hit;
-				if (Physics.Raycast(firstPersonCamera.position, firstPersonCamera.forward, out hit, 2)) {
-					if (hit.transform.gameObject.GetComponent<Rigidbody>()) {
-						objectHeld = hit.collider.gameObject;
-						objectHeld.GetComponent<Rigidbody>().isKinematic = true;
-					}
-				}
-			} else {
-				if (objectHeld.GetComponent<Rigidbody>()) {
-					objectHeld.GetComponent<Rigidbody>().isKinematic = false;
-				}
-				objectHeld = null;
-			}
-		}
-		if (objectHeld) {
-			objectHeld.transform.position = Vector3.MoveTowards(objectHeld.transform.position, transform.position + transform.forward, 7.0F * Time.deltaTime);
-			if (Input.GetKeyDown(KeyCode.Mouse2)) {
-				objectHeld.transform.eulerAngles = new Vector3(0, 90, 0);
+			RaycastHit hit;
+			if (Physics.Raycast(firstPersonCamera.position, firstPersonCamera.forward, out hit, 2)) {
+				item = hit.transform.gameObject;
+				inventoryScript.addToInventory(item.name);
 			}
 		}
 	}
