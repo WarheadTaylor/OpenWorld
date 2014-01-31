59e72f00a1d78551f47302a0be24e653d50705fe
pyrohail <pyrohail.ca@gmail.com>
2014-01-30 21:56:11 -0500

    Starting the inventory.

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory : MonoBehaviour {
+	private bool inventoryOpen = false;
+	private int[] inventoryItems;
+
+	void Start () {
+		inventoryItems = new int[20];
+	}
+
+	void Update () {
+
+	}
+
+	void onGui () {
+		GUI.Box(new Rect(0,0,640,480), "Inventory");
+	}
+}
