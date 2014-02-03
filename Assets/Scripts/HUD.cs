bfaba2fe74bb3749cf0b544153f08bd7e8d07265
pyrohail <pyrohail.ca@gmail.com>
2014-02-03 18:54:47 -0500

    Change some some syntax conventions.

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -2,27 +2,27 @@
 using System.Collections;
 
 public sealed class HUD : MonoBehaviour {
-	private int frameCount = 0;
-	private float dt = 0.0F;
-	private float fps = 0.0F;
-	private float updateRate = 4.0F;
+	private int FrameCount = 0;
+	private float DeltaTime = 0.0F;
+	private float FPS = 0.0F;
+	private float UpdateRate = 5.0F;
 
 	void Start () {
 
 	}
 
 	void Update () {
-		frameCount++;
-		dt += Time.deltaTime;
-		if (dt > 1.0F / updateRate) {
-			fps = frameCount / dt ;
-			frameCount = 0;
-			dt -= 1.0F / updateRate;
+		FrameCount++;
+		DeltaTime += Time.deltaTime;
+		if (DeltaTime > 1.0F / UpdateRate) {
+			FPS = FrameCount / DeltaTime ;
+			FrameCount = 0;
+			DeltaTime -= 1.0F / UpdateRate;
 		}
 	}
 
 	void OnGUI () {
-		GUI.Label(new Rect(10, 10, 150, 100), "Current FPS: " + ((int)fps).ToString());
+		GUI.Label(new Rect(10, 10, 150, 100), "Current FPS: " + ((int)FPS).ToString());
 		GUI.Box(new Rect(Screen.width / 2, Screen.height / 2, 0, 0), "This is a crosshair");
 	}
 }
