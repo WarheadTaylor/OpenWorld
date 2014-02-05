1c8fde8aa9b4824f52ed6e4b720915416cb5e46f
pyrohail <pyrohail.ca@gmail.com>
2014-02-05 16:13:05 -0500

    Basic structure for weapons.

---
+++ Assets/Scripts/Pickaxe.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections;
+
+public sealed class Pickaxe : Weapon {
+
+	void Start () {
+		Damage = 2.0F;
+	}
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Weapon : MonoBehaviour {
+	protected float Damage = 0.0F;
+}
