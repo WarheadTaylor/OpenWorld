7139ae050c70b7ed4aae68b491118539718d0024
pyrohail <pyrohail.ca@gmail.com>
2014-02-07 22:17:46 -0500

    AI sort of working.

---
+++ Assets/Scripts/AIPathFinding.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Pathfinding;
+
+public class AIPathFinding : MonoBehaviour {
+
+	public void Start () {
+		Seeker Seeker = GetComponent<Seeker>();
+		Seeker.StartPath(transform.position, new Vector3(0, 0, 0), OnPathComplete);
+	}
+
+	public void OnPathComplete (Path p) {
+		Utils.PyroLog("Yey, we got a path back. Did it have an error? " + p.error);
+	}
+}
