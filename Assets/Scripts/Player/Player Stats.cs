5f9fcf5010f94ab873951d372133d07ce191be88
Justin Harrison <justin@pyrohail.com>
2014-03-15 18:33:24 -0400

    Player health added.

---
+++ Assets/Scripts/Player/Player Stats.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour {
+    private int Health = 100;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+	void RemoveHealth(int amountOfHealth) {
+		Health -= amountOfHealth;
+		if (Health <= 0) {
+			Utils.PyroLog("You're dead motherfucker!");
+		}
+	}
+}
