4ab5dbf9ce7ad7a66770425d5adfc4269912be3d
Justin Harrison <justin@pyrohail.com>
2014-03-16 19:16:15 -0400

    Taylor's good at requesting things to be done, not so much at doing things.

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -4,15 +4,18 @@ using System.Collections;
 public abstract class InteractableObject : MonoBehaviour {
 	private int TimesHit = 0;
 
+	public int MaxTimesHit = 3;
+	public int AmountToCreate = 3;
+
 	public virtual void Hit (GameObject hitByObject) {
 		TimesHit++;
 
-		if (TimesHit == 3) {
+		if (TimesHit == MaxTimesHit) {
 			Destroy(transform.gameObject);
 
 			GameObject Log = Resources.Load<GameObject>("evergreenlog_01");
 			GameObject LogCreated;
-			for (int i = 0; i < 3; i++) {
+			for (int i = 0; i < AmountToCreate; i++) {
 				LogCreated = (GameObject) Instantiate(Log, transform.position + new Vector3(0, i, 0), Quaternion.identity);
 				LogCreated.rigidbody.AddForce(new Vector3(1, 0, 1), ForceMode.Impulse);
 			}
