b495342c12a5f9aaeb1aed8152ada60a06a5ae1e
me@justinharrison.ca <me@justinharrison.ca>
2021-01-27 15:33:00 -0500

    Allow changing current scene when tab is pressed

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    private static int CurrentLevel = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Input.GetKeyDown(KeyCode.Tab))
+            return;
+
+        CurrentLevel = (CurrentLevel + 1) % 2;
+
+        switch (CurrentLevel)
+        {
+            case 0:
+                SceneManager.LoadScene("MainScene");
+                break;
+            case 1:
+                SceneManager.LoadScene("PyroTestScene");
+                break;
+        }
+    }
+}
